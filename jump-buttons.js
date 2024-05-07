27c1713a1d133dfdd1d6147a41cdde39d0ed9260
Simon Wiles <simonjwiles@gmail.com>
2024-05-06 17:17:32 -0700

    Genericize skip functions a little

--- jump-buttons.js
+++ jump-buttons.js
@@ -1,20 +1,20 @@
-const scrollToPrev = (el) => {
+const scrollToPrev = (el, selector, vOffset = 20) => {
   const currentScroll = el.scrollTop;
-  for (const highlight of [...el.querySelectorAll(".highlight")].toReversed()) {
-    if (highlight.offsetTop < currentScroll) {
-      el.scrollTo({ top: highlight.offsetTop, behavior: "smooth" });
-      flashEl(highlight);
+  for (const subEl of [...el.querySelectorAll(selector)].toReversed()) {
+    if (subEl.offsetTop - vOffset < currentScroll) {
+      el.scrollTo({ top: subEl.offsetTop - vOffset, behavior: "smooth" });
+      flashEl(subEl);
       break;
     }
   }
 };
 
-const scrollToNext = (el) => {
+const scrollToNext = (el, selector, vOffset = 20) => {
   const currentScroll = el.scrollTop;
-  for (const highlight of el.querySelectorAll(".highlight")) {
-    if (highlight.offsetTop > currentScroll) {
-      el.scrollTo({ top: highlight.offsetTop, behavior: "smooth" });
-      flashEl(highlight);
+  for (const subEl of el.querySelectorAll(selector)) {
+    if (subEl.offsetTop - vOffset > currentScroll) {
+      el.scrollTo({ top: subEl.offsetTop - vOffset, behavior: "smooth" });
+      flashEl(subEl);
       break;
     }
   }
@@ -23,23 +23,23 @@ const scrollToNext = (el) => {
 document
   .getElementById("t-prev")
   .addEventListener("click", () =>
-    scrollToPrev(document.getElementById("t-text"))
+    scrollToPrev(document.getElementById("t-text"), ".highlight")
   );
 
 document
   .getElementById("t-next")
   .addEventListener("click", () =>
-    scrollToNext(document.getElementById("t-text"))
+    scrollToNext(document.getElementById("t-text"), ".highlight")
   );
 
 document
   .getElementById("ls-prev")
   .addEventListener("click", () =>
-    scrollToPrev(document.getElementById("ls-text"))
+    scrollToPrev(document.getElementById("ls-text"), ".highlight")
   );
 
 document
   .getElementById("ls-next")
   .addEventListener("click", () =>
-    scrollToNext(document.getElementById("ls-text"))
+    scrollToNext(document.getElementById("ls-text"), ".highlight")
   );
